acc68a4473acead37836504d19b3d65ef669f28a
Dimitrije <rdomiran@gmail.com>
2022-04-11 23:10:28 +0200

    radi i opciono

--- myscript.js
+++ myscript.js
@@ -31,11 +31,38 @@ function draw(){
     }
 };
 
+function RRGGBB() {
+    let mora = '';
+    let nizic = [];
+    for(let i = 0; i < 3; i++){
+        nizic[i] = Math.floor(Math.random() * (255 - 0));
+    }
+    mora = nizic.toString();
+    return "rgb(" + mora + ")";
+}
+
+function randRGB(komadic){
+    let original = komadic.slice(4,komadic.length-1);
+    let karera = original.split(',');
+    let mora = '';
+    for(let i = 0; i < karera.length; i++){
+        karera[i] = Math.floor(karera[i]*0.9);
+    }
+    mora =  karera.toString();
+    return "rgb(" + mora + ")";
+}
+
 function hoveraj(){
 const hover_targ = document.querySelectorAll('.container div div');
 hover_targ.forEach((field) => {
-    field.addEventListener('mouseover',() =>{
-        field.style.backgroundColor = 'black';
+    field.addEventListener('mouseover',(current,previous) =>{
+        if(field.style.backgroundColor == ''){
+            field.style.backgroundColor = RRGGBB();
+        }else{
+            previous = field.style['background-color'];
+            current = randRGB(previous);
+            field.style.backgroundColor = current;
+        }
     }); 
 });
 };
