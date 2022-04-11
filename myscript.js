0e6cdbefd8f703a461c4c7d03ed9889b0c3cc3e9
Dimitrije <rdomiran@gmail.com>
2022-04-11 18:08:11 +0200

    preostala je samo optional opcija

--- myscript.js
+++ myscript.js
@@ -1,12 +1,19 @@
 const container = document.querySelector(".container");
 const body = document.querySelector('body');
 
-let tab_width = 16;
+let tab_width = 10;
 let tab_height = tab_width;
 
 draw();
+hoveraj();
 
 function draw(){
+    //proveri za vec postojeci grid, pa ga izbrisi
+    let element = document.querySelector(".container");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+    //ovaj kod ispod crta novi grid
     tab_height = tab_width;
     for(i = 0; i < tab_width; i++){
         const grid_r = document.createElement("div");
@@ -22,12 +29,28 @@ function draw(){
             grid_r.appendChild(grid_el);
         }
     }
-}
+};
 
+function hoveraj(){
 const hover_targ = document.querySelectorAll('.container div div');
 hover_targ.forEach((field) => {
     field.addEventListener('mouseover',() =>{
         field.style.backgroundColor = 'black';
     }); 
+});
+};
+
+//Zasto ovo ne radi kada nije u funkciji??
+//To se odnosi na funkciju hoveraj
+
+const btn = document.querySelector('.rst');
+btn.addEventListener('click', ()=>{
+    tab_width = prompt("Insert table width");
+    if(tab_width > 100){
+        tab_width = 100;
+    }
+    console.log(tab_width);
+    draw();
+    hoveraj();
 });
 
